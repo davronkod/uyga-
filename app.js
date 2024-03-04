dbe30fda3db0513f3416c069ea8579eb226fe784
davronkod <155731844+davronkod@users.noreply.github.com>
2024-03-04 13:54:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+alert('assalomu alaykum')
+confirm('hush kelibsiz')
+prompt('iltimos ismingiz')
+
+
+console.log('1');
+var num1 = "1"
+console.log('2');
+var num2 = "2"
+console.log('3');
+var num3 = "3"
+console.log('4');
+var num4 = "4"
+console.log('5');
+var num5 = "5"
+console.log('6');
+var num6 = "6"
+console.log('7');
+var num7 = "7"
+console.log('8');
+var num8 = "8"
+console.log('9');
+var num9 = "9"
+console.log('10');
+var num10 = "10"
+let name1 = "names";
+console.log(name1);
+let name2 = "iso";
+console.log(name2);
+let name3 = "bekish";
+console.log(name3);
+let name4 = "davronbek";
+console.log(name4);
+let name5 = "shodi";
+console.log(name5);
+let name6 = "ulug";
+console.log(name6);
+let name7 = "bobur";
+console.log(name7);
+let name8 = "salm";
+console.log(name8);
+let name9 = "point";
+console.log(name9);
+let name10 = "power";
+console.log(name10);
+const male = "exel"
+console.log(male);
+const male1 = "vort"
+console.log(male1);
+const male2 = "toxic"
+console.log(male2);
+const male3 = "master"
+console.log(male3);
+const male4 = "aca"
+console.log(male4);
+const male5 = "besh"
+console.log(male5);
+const male6 = "class"
+console.log(male6);
+const male7 = "deyap"
+console.log(male7);
+const male8 = "nima"
+console.log(male8);
+const male9 = "siz"
+console.log(male9);
+const male10 = "biz"
+console.log(male10);
